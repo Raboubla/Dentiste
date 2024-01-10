6dc83b7886525fda7bb188125cf9f1cb97d784a0
Raboubla <hasinkasina@gmail.com>
2024-01-10 20:55:49 +0300

    fenetre misy zavatra

--- Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af306a3497c462b8ff8e9b30e34f24412359a090")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
